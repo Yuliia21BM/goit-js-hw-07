5e7afff34df9c2e8fb245c76d37fb282c30763ad
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2022-10-16 20:53:56 +0300

    Add firt gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const gallerySet = createGalleryset(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', gallerySet);
+
+galleryRef.addEventListener('click', onAddItemsClick);
+
+function createGalleryset(items =[]) {
+    return items.map((item) => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+  </a>
+</div>`).join("");
+}
+
+function onAddItemsClick(evt) {
+    evt.preventDefault();
+    const itemSrc = evt.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${itemSrc}" width="800" height="600">
+    `);
+    instance.show( (instance) => {
+        document.onkeydown = function (evt) {
+            evt = evt || window.event;
+            var isEscape = false;
+            if ("key" in evt) {
+                isEscape = (evt.key === "Escape" || evt.key === "Esc");
+            } else {
+                isEscape = (evt.keyCode === 27);
+            }
+            if (isEscape) {
+                instance.close();
+            }
+        };
+})
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+const gallerySet = createGalleryset(galleryItems);
+galleryRef.insertAdjacentHTML('beforeend', gallerySet);
+
+galleryRef.addEventListener('click', onAddItemsClick);
+
+function createGalleryset(items =[]) {
+    return items.map((item) => 
+    `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`).join("");
+}
+
+function onAddItemsClick(evt) {
+    evt.preventDefault();
+    const itemSrc = evt.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img src="${itemSrc}" width="800" height="600">
+    `);
+    instance.show( (instance) => {
+        document.onkeydown = function (evt) {
+            evt = evt || window.event;
+            var isEscape = false;
+            if ("key" in evt) {
+                isEscape = (evt.key === "Escape" || evt.key === "Esc");
+            } else {
+                isEscape = (evt.keyCode === 27);
+            }
+            if (isEscape) {
+                instance.close();
+            }
+        };
+})
+}
+
