a0d81f37ccf6099b7c40d1cd9614e1bf7901f73d
Yuliia Myhailiuk <myhailiukyulia@gmail.com>
2022-10-17 20:13:13 +0300

    Add second galery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -17,23 +17,6 @@ function createGalleryset(items =[]) {
 
 function onAddItemsClick(evt) {
     evt.preventDefault();
-    const itemSrc = evt.target.dataset.source;
-    const instance = basicLightbox.create(`
-    <img src="${itemSrc}" width="800" height="600">
-    `);
-    instance.show( (instance) => {
-        document.onkeydown = function (evt) {
-            evt = evt || window.event;
-            var isEscape = false;
-            if ("key" in evt) {
-                isEscape = (evt.key === "Escape" || evt.key === "Esc");
-            } else {
-                isEscape = (evt.keyCode === 27);
-            }
-            if (isEscape) {
-                instance.close();
-            }
-        };
-})
 }
 
+let gallery = new SimpleLightbox('.gallery a', {captionsData: 'alt', captionDelay: 250,});
